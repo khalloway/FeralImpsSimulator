4f256a1c926319127f4304233c08f7980fc01449
Keiji <keiji.halloway@gmail.com>
2022-11-23 10:17:36 -0600

    Initial commit

---
+++ src/main/java/com/kotfi/FeralImpsSimulator/FeralImpsSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.kotfi.FeralImpsSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeralImpsSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeralImpsSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kotfi/FeralImpsSimulator/FeralImpsSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotfi.FeralImpsSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeralImpsSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
